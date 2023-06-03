7ef88495b6038829864c26dca3e94109347aef44
fallfire13 <47854729+fallfire13@users.noreply.github.com>
2023-06-03 17:01:06 +0300

    Add files via upload

---
+++ FP/lab4/main4.lisp
@@ -0,0 +1,31 @@
+(defun single-arg (expr)
+  (and (not (null expr)) (not (null (rest expr))) (null (rest (rest expr))))
+)
+
+(defun form-to-postfix (expr)
+  (if (listp expr)
+      (if (single-arg expr)
+          (if (eq (first expr) '- )
+              (concatenate 'string "0 "(princ-to-string (form-to-postfix (second expr))) " -")
+              (concatenate 'string "1 " (princ-to-string (form-to-postfix (second expr))) " /")
+          )
+          (concatenate 'string (form-to-postfix (second expr)) " "
+                               (proc-args (first expr) (rest (rest expr))))
+      )
+      (string-downcase (princ-to-string expr))
+  )
+)
+
+(defun proc-args (operator expr)
+  (if (not (null expr))
+      (if (null (rest expr))
+          (concatenate 'string (form-to-postfix (first expr)) " " 
+                               (princ-to-string operator)) 
+          (concatenate 'string (form-to-postfix (first expr)) " " 
+                               (princ-to-string operator) " " 
+                               (proc-args operator (rest expr)))
+      )
+  )
+)
+
+(defun t (expr) (form-to-postfix expr))
